49a227a87d7c373afd71a6981fc40af1df18c456
Vika <vikodavido@gmail.com>
2024-12-16 08:32:35 +0200

    homework-9

--- index.php
+++ index.php
@@ -1,12 +1,75 @@
 <?php
-    $dsn = 'mysql:host=db;dbname=car_service;charset=utf8';
-    $user = 'root';
-    $password = 'secret';
 
-    try {
-        $pdo = new PDO($dsn, $user, $password);
-        echo "Successful connection!";
-    } catch (PDOException $e) {
-        echo "Connection error: " . $e->getMessage();
+class Logger
+{
+
+    private $format;
+    private $delivery;
+
+    public function __construct($format, $delivery)
+    {
+        $this->format   = $format;
+        $this->delivery = $delivery;
     }
+
+    public function log($string)
+    {
+        $this->deliver($this->format($string));
+    }
+
+    public function format($string)
+    {
+        switch ($this->format) {
+            case 'raw' :
+                {
+                    return $string;
+                }
+                break;
+            case 'with_date':
+                {
+                    return date('Y-m-d H:i:s') . $string;
+                }
+                break;
+            case 'with_date_and_details':
+                {
+                    return date('Y-m-d H:i:s') . $string . ' - With some details';
+                }
+                break;
+            default:
+            {
+                die('Error format');
+            }
+        }
+    }
+
+    public function deliver($format)
+    {
+        switch ($this->delivery) {
+            case 'by_email' :
+                {
+                    echo "Вывод формата ({$format}) по имейл";
+                }
+                break;
+            case 'by_sms':
+                {
+                    echo "Вывод формата ({$format}) в смс";
+                }
+                break;
+            case 'to_console':
+                {
+                    echo "Вывод формата ({$format}) в консоль";
+                }
+                break;
+            default:
+            {
+                die('Error deliver');
+            }
+        }
+    }
+
+}
+
+$logger = new Logger('raw', 'by_sms');
+$logger->log('Emergency error! Please fix me!');
+
 ?>
