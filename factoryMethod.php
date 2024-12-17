1b7c63848dcf1bc0a94eb826484312bf991d2fee
Vika <vikodavido@gmail.com>
2024-12-17 19:16:09 +0200

    fix

--- factoryMethod.php
+++ factoryMethod.php
@@ -1,51 +1,74 @@
 <?php
 
-class BaseTaxi {
-  public function getCarModel() {}
-
-  public function getPrice() {}
+interface TaxiInterface {
+  public function getCarModel(): string;
+  public function getPrice(): int;
 }
 
-class EconomTaxi extends BaseTaxi {
-  public function getCarModel() {
+abstract class BaseTaxi {
+  abstract public function createTaxi(): TaxiInterface;
+}
+
+class EconomTaxi implements TaxiInterface {
+  public function getCarModel(): string {
     return "Econom Car Model";
   }
 
-  public function getPrice() {
+  public function getPrice(): int {
     return 30;
   }
 }
 
-class StandardTaxi extends BaseTaxi {
-  public function getCarModel() {
+class StandardTaxi implements TaxiInterface {
+  public function getCarModel(): string {
     return "Standard Car Model";
   }
 
-  public function getPrice() {
+  public function getPrice(): int {
     return 50;
   }
 }
 
-class LuxuryTaxi extends BaseTaxi {
-  public function getCarModel() {
+class LuxuryTaxi implements TaxiInterface {
+  public function getCarModel(): string {
     return "Luxury Car Model";
   }
 
-  public function getPrice() {
+  public function getPrice(): int {
     return 80;
   }
 }
 
-$myEconomTaxi = new EconomTaxi();
-echo $myEconomTaxi->getCarModel();  
-echo $myEconomTaxi->getPrice();  
+class EconomTaxiFactory extends BaseTaxi {
+  public function createTaxi(): TaxiInterface {
+    return new EconomTaxi();
+  }
+}
 
-$myStandardTaxi = new StandardTaxi();
-echo $myStandardTaxi->getCarModel(); 
-echo $myStandardTaxi->getPrice();   
+class StandardTaxiFactory extends BaseTaxi {
+  public function createTaxi(): TaxiInterface {
+    return new StandardTaxi();
+  }
+}
+class LuxuryTaxiFactory extends BaseTaxi {
+  public function createTaxi(): TaxiInterface {
+    return new LuxuryTaxi();
+  }
+}
 
-$myLuxuryTaxi = new LuxuryTaxi();
-echo $myLuxuryTaxi->getCarModel();  
-echo $myLuxuryTaxi->getPrice();    
+$economFactory = new EconomTaxiFactory();
+$economTaxi = $economFactory->createTaxi();
+echo $economTaxi->getCarModel() . "\n"; 
+echo $economTaxi->getPrice() . "\n";   
+
+$standardFactory = new StandardTaxiFactory();
+$standardTaxi = $standardFactory->createTaxi();
+echo $standardTaxi->getCarModel() . "\n"; 
+echo $standardTaxi->getPrice() . "\n"; 
+
+$luxuryFactory = new LuxuryTaxiFactory();
+$luxuryTaxi = $luxuryFactory->createTaxi();
+echo $luxuryTaxi->getCarModel() . "\n"; 
+echo $luxuryTaxi->getPrice() . "\n"; 
 
 ?>
